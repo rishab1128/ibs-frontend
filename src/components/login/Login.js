5c037b4d96843c7a44a39786e39f09166920ba4d
rishab1128 <rishabpurkayastha28@gmail.com>
2023-08-09 21:43:07 +0530

    Login/Signup routes added

---
+++ src/components/login/Login.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Add your login logic here
+    console.log('Logging in...');
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit}>
+        <h2>Login</h2>
+        <div className="form-group">
+          <label>Email:</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="form-control"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Login
+        </button>
+        <p>Don't have an account? <Link to="/signup">Sign up</Link></p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
