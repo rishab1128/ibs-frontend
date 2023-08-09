d3fdaa787e763bcdcb732d8f910bfcdb4ba77f10
rishab1128 <rishabpurkayastha28@gmail.com>
2023-08-09 22:15:51 +0530

    Home component styling added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/login/Login'
 import SignUp from './components/signup/SignUp';
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,10 +1,39 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Home.css';
 
 const Home = () => {
   return (
-    <div>
-      <h2>Welcome to the Home Page</h2>
-      <p>This is the main page of your application.</p>
+    <div className="home-container">
+      <header className="home-header">
+        <h1>Welcome to MyBank</h1>
+        <p>Your Trusted Banking Partner</p>
+      </header>
+      <main className="home-content">
+        <div className="feature-box">
+          <h2>Online Banking</h2>
+          <p>Manage your finances from anywhere, anytime.</p>
+        </div>
+        <div className="feature-box">
+          <h2>Secure Transactions</h2>
+          <p>Your transactions are safe and encrypted.</p>
+        </div>
+        <div className="feature-box">
+          <h2>24/7 Customer Support</h2>
+          <p>We're here to assist you around the clock.</p>
+        </div>
+      </main>
+      <div className="login-signup-section">
+        <div className="login-section">
+          <h2><Link to="/login">Login</Link></h2>
+        </div>
+        <div className="signup-section">
+          <h2><Link to="/signup">SignUp</Link></h2>
+        </div>
+      </div>
+      <footer className="home-footer">
+        <p>&copy; 2023 MyBank. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
